3b9a66f87e3f9e45bbfa1198342dfb1746c797b7
Tanya <misterioremo@gmail.com>
2020-04-13 12:24:08 +0300

    Класс для работы с виджетов-блюпринтов.

---
+++ Source/SummerIce/Util/WidgetLibrary.cpp
@@ -0,0 +1,17 @@
+﻿#include "WidgetLibrary.h"
+#include "Blueprint/UserWidget.h"
+#include "UObject/ConstructorHelpers.h"
+
+TSubclassOf<UUserWidget> UWidgetLibrary::DialogBubbleClass;
+
+UWidgetLibrary::UWidgetLibrary()
+{
+	// исходная ссылка WidgetBlueprint'/Game/Widget/W_DialogWindow.W_DialogWindow'
+	// в ней нужно изменить WidgetBlueprint -> WidgetBlueprintGeneratedClass
+	// и *.W_DialogWindow -> *.W_DialogWindow_C
+
+	if (!DialogBubbleClass) {
+		static ConstructorHelpers::FClassFinder<UUserWidget> Finded(TEXT("WidgetBlueprintGeneratedClass'/Game/Widget/W_Bubble.W_Bubble_C'"));
+		if (Finded.Succeeded())	DialogBubbleClass = Finded.Class;
+	}
+}
---
+++ Source/SummerIce/Util/WidgetLibrary.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "WidgetLibrary.generated.h"
+
+class UUserWidget;
+
+UCLASS()
+class SUMMERICE_API UWidgetLibrary : public UObject
+{
+	GENERATED_BODY()
+
+private:
+protected:
+public:
+	UWidgetLibrary();
+
+private:
+protected:
+public:
+	static TSubclassOf<UUserWidget> DialogBubbleClass;
+};
