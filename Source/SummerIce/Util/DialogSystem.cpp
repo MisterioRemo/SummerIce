cc2e5dd32af8a2adec89f268691ab5d707280a7b
Tanya <misterioremo@gmail.com>
2020-04-13 12:17:40 +0300

    Система управления диалогами.

---
+++ Source/SummerIce/Util/DialogSystem.cpp
@@ -0,0 +1,131 @@
+﻿#include "DialogSystem.h"
+
+DialogTree* DialogSystem::_DialogTree;
+
+DialogTree::DialogMap* DialogSystem::_Dialog;
+int32 DialogSystem::_DialogId;
+DialogNode* DialogSystem::_Node;
+
+TArray<int32>* DialogSystem::_LaviniaNodeArray;
+int32 DialogSystem::_LaviniaNodeId;
+
+TMap<ECharacter, IInteractInterface*> DialogSystem::_Speakers;
+TMap<int32, int32> DialogSystem::_DialogNodeMap;
+
+
+
+void DialogSystem::LoadDialogTree(const int32 & LvlId)
+{
+	_DialogTree = new DialogTree();
+	_DialogTree->LoadDialogFromJson(LvlId);
+	_DialogNodeMap.Empty();
+	RemoveSpeakers();
+}
+
+void DialogSystem::AddSpeaker(const IInteractInterface * Actor)
+{
+	if (Actor) _Speakers.Emplace(Actor->GetName(), const_cast<IInteractInterface*>(Actor));	
+}
+
+void DialogSystem::RemoveSpeaker(const IInteractInterface * Actor)
+{
+	if (Actor) _Speakers.Remove(Actor->GetName());
+}
+
+void DialogSystem::RemoveSpeakers()
+{
+	_Speakers.Empty();
+
+	_LaviniaNodeArray = nullptr;
+	_LaviniaNodeId = 0;
+
+	_Dialog = nullptr;
+	_DialogId = -1;
+	_Node = nullptr;
+}
+
+bool DialogSystem::IsDialogValid()
+{
+	if (!_Dialog || _DialogId == -1) {
+		for (const auto& Speaker : _Speakers) {
+			_DialogId = Speaker.Value->GetDialogId();
+			_Dialog = _DialogTree->GetDialogById(_DialogId);
+			if (_Dialog) return true;			
+		}
+	}
+	else {
+		return true;
+	}
+
+	return false;
+}
+
+bool DialogSystem::IsNodeValid()
+{
+	int32 NodeId = _DialogNodeMap.FindOrAdd(_DialogId);
+	_Node = _Dialog->Find(NodeId);
+
+	if (_Node) {
+		if (_Node->Speaker != ECharacter::Lavinia && _Node->Answers.Num() > 1) {
+			_LaviniaNodeArray = &_Node->Answers;
+		}
+		else if (_LaviniaNodeArray && !_LaviniaNodeArray->Contains(NodeId)) {
+			_LaviniaNodeArray = nullptr;
+			_LaviniaNodeId = 0;
+		}
+
+		return true;
+	}
+
+	return false;	
+}
+
+void DialogSystem::ShowCurrentNode()
+{
+	if (const auto& Actor = _Speakers.Find(_Node->Speaker)) {
+		bool bCanChoose = (_LaviniaNodeArray) ? true : false;
+		(*Actor)->ShowDialogWidget(&_Node->Speech, bCanChoose);
+		ChooseNextNode();
+	}
+}
+
+void DialogSystem::ChooseNextNode()
+{
+	int32 Size = _Node->Answers.Num();
+
+	// если говорит игрок, то выбрать следующей нодой случайный ответ нпс
+	// в противном случае выбирается первый доступный
+	if (_Node->Speaker == ECharacter::Lavinia && Size > 1)
+		_DialogNodeMap[_DialogId] = _Node->Answers[FMath::Rand() % Size];
+	else
+		_DialogNodeMap[_DialogId] = (Size > 0) ? _Node->Answers[0] : -1;
+}
+
+void DialogSystem::NextNode()
+{
+	if (_LaviniaNodeArray) {
+		_LaviniaNodeId = (_LaviniaNodeId + 1) % _LaviniaNodeArray->Num();
+		_Node = _Dialog->Find((*_LaviniaNodeArray)[_LaviniaNodeId]);
+		ShowCurrentNode();
+	}
+}
+
+void DialogSystem::PrevNode()
+{
+	if (_LaviniaNodeArray) {
+		_LaviniaNodeId = (_LaviniaNodeId > 0) ? (_LaviniaNodeId - 1) : (_LaviniaNodeArray->Num() - 1);
+		_Node = _Dialog->Find((*_LaviniaNodeArray)[_LaviniaNodeId]);
+		ShowCurrentNode();
+	}
+}
+
+bool DialogSystem::StartOrContinueDialog()
+{
+	for (const auto& Speaker : _Speakers) Speaker.Value->HideDialogWidget();	
+
+	if (IsDialogValid() && IsNodeValid()) {
+		ShowCurrentNode();
+		return true;
+	}
+	return false;
+}
---
+++ Source/SummerIce/Util/DialogSystem.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "Util/DialogTree.h"
+#include "Interface/InteractInterface.h"
+
+class ASummerIceGameModeBase;
+
+class SUMMERICE_API DialogSystem
+{
+
+	friend ASummerIceGameModeBase;
+
+private:
+	static void LoadDialogTree(const int32 & LvlId);
+	static bool IsDialogValid();
+	static bool IsNodeValid();
+	static void ShowCurrentNode();
+	static void ChooseNextNode();
+
+protected:
+public:
+	static void AddSpeaker(const IInteractInterface * Actor);
+	static void RemoveSpeaker(const IInteractInterface * Actor);
+	static void RemoveSpeakers();
+
+	static void NextNode();
+	static void PrevNode();
+
+	static bool StartOrContinueDialog();
+
+private:
+	static DialogTree* _DialogTree;					// содержит все диалоги уровня
+	static DialogTree::DialogMap* _Dialog;			// текущий диалог (содержит все узлы)	
+	static int32 _DialogId;
+	static DialogNode* _Node;						// текущий узел
+
+	static int32 _LaviniaNodeId;
+	static TArray<int32>* _LaviniaNodeArray;
+
+	static TMap<ECharacter, IInteractInterface*> _Speakers;		// участники диалога, попадают по триггеру, удаляются так же
+	static TMap<int32, int32> _DialogNodeMap;					// dialogId - nodeId
+																// свзязь между диалогом и нодой,
+																// на которой закончился разговор в последний раз
+protected:
+public:
+};
