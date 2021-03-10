524d6e366c8de6430c82612761fd47bca5ddd405
Tanya <misterioremo@gmail.com>
2021-03-10 20:16:34 +0300

    Смена flipbook выключателя при нажатии пользователем.

--- Source/SummerIce/World/LightSwitch.cpp
+++ Source/SummerIce/World/LightSwitch.cpp
@@ -1,15 +1,21 @@
-#include "LightSwitch.h"
+﻿#include "LightSwitch.h"
+#include "PaperFlipbookComponent.h"
 
 void ALightSwitch::BeginPlay()
 {
   Super::BeginPlay();
   _EventType = EGameEventType::Other;
   _EventTiming = EActionTiming::PressInteractBtn;
+
+  // применить настройки bTurnOn из эдитора
+  bTurnOn = !bTurnOn;
+  ChooseEvent();
 }
 
 void ALightSwitch::ChooseEvent()
 {
   bTurnOn = !bTurnOn;
+  _BodyFlipbookComponent->SetPlaybackPositionInFrames(bTurnOn ? 0 : 1, false);
   for (const auto Light : FakeLights) {
     Light->SwitchLightState(bTurnOn);
   }
